4bc64439e9b84c52b9f2bdf943ccd6952c0f1d64
cheef78 <suslovoleg@mail.ru>
2019-02-06 00:41:16 +0300

    проработал логику выигрыша. Осталась проблема завершения цикла

--- less8_normal.py
+++ less8_normal.py
@@ -2,7 +2,34 @@ import sys
 import random
 import curses
 
-
+Viigr1 = [1,2,3,4,5]
+Viigr2 = [6,7,8,9,10]
+Viigr3 = [11,12,13,14,15]
+Viigr4 = [16,17,18,19,20]
+Viigr5 = [21,22,23,24,25]
+Viigr6 = [1,6,11,16,21]
+Viigr7 = [2,7,12,17,22]
+Viigr8 = [3,8,13,18,23]
+Viigr9 = [4,8,14,19,24]
+Viigr10 = [5,10,15,20,25]
+Viigr11 = [1,7,13,19,25]
+Viigr12 = [5,9,13,17,21]
+viigrAll =[]
+viigrAll.append(Viigr1)
+viigrAll.append(Viigr2)
+viigrAll.append(Viigr3)
+viigrAll.append(Viigr4)
+viigrAll.append(Viigr5)
+viigrAll.append(Viigr6)
+viigrAll.append(Viigr7)
+viigrAll.append(Viigr8)
+viigrAll.append(Viigr9)
+viigrAll.append(Viigr10)
+viigrAll.append(Viigr11)
+viigrAll.append(Viigr12)
+for el in range (0,12):
+    print (viigrAll[int(el)])
+    
 print ()
 print ()
 print ("Игра КРЕСТИКИ - НОЛИКИ")
@@ -88,51 +115,70 @@ print ("Отлично!! Начинаем ИГРАТЬ !!")
 print ()
 print ()
 
-all = []
+all1 = []
 for el in range (1,26):
-    all.append(int(el))
+    all1.append(int(el))
 
 print(" *  *  *  *  *  * * * *")
-print(f" *  {all[0]}   {all[1]}   {all[2]}   {all[3]}   {all[4]}  *")
-print(f" *  {all[5]}   {all[6]}   {all[7]}   {all[8]}  {all[9]} *")
-print(f" * {all[10]}  {all[11]}  {all[12]}  {all[13]}  {all[14]} *")
-print(f" * {all[15]}  {all[16]}  {all[17]}  {all[18]}  {all[19]} *")
-print(f" * {all[20]}  {all[21]}  {all[22]}  {all[23]}  {all[24]} *")
+print(f" *  {all1[0]}   {all1[1]}   {all1[2]}   {all1[3]}   {all1[4]}  *")
+print(f" *  {all1[5]}   {all1[6]}   {all1[7]}   {all1[8]}  {all1[9]} *")
+print(f" * {all1[10]}  {all1[11]}  {all1[12]}  {all1[13]}  {all1[14]} *")
+print(f" * {all1[15]}  {all1[16]}  {all1[17]}  {all1[18]}  {all1[19]} *")
+print(f" * {all1[20]}  {all1[21]}  {all1[22]}  {all1[23]}  {all1[24]} *")
 print(" *  *  *  *  *  * * * *")
 
-
-for el1 in range (1,28):
-    num = int(input("Игрок "+plrname1+ "введите номер ячейки, куда хотите поставить" + plrchar1))
-    all[num-1]=plrchar1
+plrnumb1=[]
+plrnumb2=[]
+plrnumbAll=[]
+el1=0
+while len(plrnumbAll) <= 25:
+    num = 0
+    el1 = el1+1
+    num = int(input("Игрок_"+plrname1+ "_введите номер ячейки, куда хотите поставить_" + plrchar1+": "))
+    all1[num-1]=plrchar1
     print(" *  *  *  *  *  * * * *")
-    print(f" *  {all[0]}   {all[1]}   {all[2]}   {all[3]}   {all[4]}  *")
-    print(f" *  {all[5]}   {all[6]}   {all[7]}   {all[8]}  {all[9]} *")
-    print(f" * {all[10]}  {all[11]}  {all[12]}  {all[13]}  {all[14]} *")
-    print(f" * {all[15]}  {all[16]}  {all[17]}  {all[18]}  {all[19]} *")
-    print(f" * {all[20]}  {all[21]}  {all[22]}  {all[23]}  {all[24]} *")
+    print(f" *  {all1[0]}   {all1[1]}   {all1[2]}   {all1[3]}   {all1[4]}  *")
+    print(f" *  {all1[5]}   {all1[6]}   {all1[7]}   {all1[8]}  {all1[9]} *")
+    print(f" * {all1[10]}  {all1[11]}  {all1[12]}  {all1[13]}  {all1[14]} *")
+    print(f" * {all1[15]}  {all1[16]}  {all1[17]}  {all1[18]}  {all1[19]} *")
+    print(f" * {all1[20]}  {all1[21]}  {all1[22]}  {all1[23]}  {all1[24]} *")
     print(" *  *  *  *  *  * * * *")
 
+    plrnumb1.append(int(num))
+    plrnumb1=sorted(plrnumb1)
+    for el in range (0,12):
+        if set(viigrAll[int(el)]).issubset(plrnumb1)==True:# Входит ли list_1 в list_2 ?
+            print ("ИГРОК "+ plrname1 + " ПОЗДРАВЛЯЕМ ВАС! ВЫ ВЫИГРАЛИ!!! УРА!!!")
+            sys.exit(0)
+    print (plrnumb1)
+    
+    print (el1)
+    el1 = el1+2
+    num = 0
+    num = int(input("Игрок_"+plrname2+ "_введите номер ячейки, куда хотите поставить_" + plrchar2+": "))
+    all1[num-1]=plrchar2
+    print(" *  *  *  *  *  * * * *")
+    print(f" *  {all1[0]}   {all1[1]}   {all1[2]}   {all1[3]}   {all1[4]}  *")
+    print(f" *  {all1[5]}   {all1[6]}   {all1[7]}   {all1[8]}  {all1[9]} *")
+    print(f" * {all1[10]}  {all1[11]}  {all1[12]}  {all1[13]}  {all1[14]} *")
+    print(f" * {all1[15]}  {all1[16]}  {all1[17]}  {all1[18]}  {all1[19]} *")
+    print(f" * {all1[20]}  {all1[21]}  {all1[22]}  {all1[23]}  {all1[24]} *")
+    print(" *  *  *  *  *  * * * *")
 
-
-
-print("Извините, но дальше код не работает.\nЕсли вы хотите продолжения данного проекта,\nто обратитесь к КИВИКошельку Разработчика.\nЗаранее спасибо!!")
+    plrnumb2.append(int(num))
+    plrnumb2=sorted(plrnumb2)
+    for el in range (0,12):
+        if set(viigrAll[int(el)]).issubset(plrnumb2)==True:# Входит ли list_1 в list_2 ?
+            print ("ИГРОК "+ plrname1 + " ПОЗДРАВЛЯЕМ ВАС! ВЫ ВЫИГРАЛИ!!! УРА!!!")
+            sys.exit(0)
+    print (plrnumb2)
+    print (el1)
+    plrnumbAll= plrnumb1+plrnumb2
+    plrnumbAll=sorted(plrnumbAll)
+    print (plrnumbAll)
+    print (len(plrnumbAll))
+print("УВАЖАЕМЫЕ ИГРОКИ! ИЗВИНИТЕ, НО БОЕВАЯ НИЧЬЯ!!!\nПОБЕДИЛА ДРУЖБА!! СПАСИБО ЗА ИГРУ!!")
 print("")
-#sys.exit(0)
-
-
-
-
-
-Viigr1 = [1,2,3,4,5]
-Viigr2 = [6,7,8,9,10]
-Viigr3 = [11,12,13,14,15]
-Viigr4 = [16,17,18,19,20]
-Viigr5 = [21,22,23,24,25]
-Viigr6 = [1,6,11,16,21]
-Viigr7 = [2,7,12,17,22]
-Viigr8 = [3,8,13,18,23]
-Viigr9 = [4,8,14,19,24]
-Viigr10 = [5,10,15,20,25]
-Viigr11 = [1,7,13,19,25]
-Viigr12 = [5,9,13,17,21]
-
+sys.exit(0)
+    
+        
